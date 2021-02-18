103634d151657bddfebc14e255330813f032382f
Arnizack <chris-libner@t-online.de>
2021-02-18 17:37:07 +0100

    MonteCarlo functional style

--- src/jpc_tracer/core/MonteCarlo.cpp
+++ src/jpc_tracer/core/MonteCarlo.cpp
@@ -7,10 +7,4 @@ namespace jpc_tracer
 {
     
 
-    Prec BalanceHeuristic(const Prec* pdf_first,const Prec* pdf_last, Prec* relative_count,int distribution_idx) 
-    {
-        Prec sum = std::inner_product(pdf_first,pdf_last,relative_count,0.f);
-
-        return pdf_first[distribution_idx]*relative_count[distribution_idx] / sum;
-    }
 }
--- src/jpc_tracer/core/MonteCarlo.h
+++ src/jpc_tracer/core/MonteCarlo.h
@@ -6,7 +6,12 @@
 #include <functional>
 #include <iterator>
 #include <numeric>
+#include <tuple>
+#include <type_traits>
 #include <vector>
+#include<concepts>
+
+
 
 namespace jpc_tracer
 {
@@ -19,62 +24,90 @@ namespace jpc_tracer
         return result/n;
     }
 
-    /*
-    Vals    Pdf           Dist_Idx                    
-    ....    ...|... |...  |
+    template<class T,class ReturnT, class...Args >
+    concept Functor = std::is_convertible<T, std::function<ReturnT(Args... )>>::value;
 
-    relative_count 
-    ....
-    */
+    namespace MIS {
+        template<class T,class InT>
+        concept Pdf = Functor<T, Prec, InT>;
 
-    template<class T>
-    struct MISSamples
-    {
-        T Value;
-        std::vector<Prec> Pdfs;
-        int DistributionIdx; 
+        template<class T, class OutT, class InT>
+        concept Func = Functor<T,OutT,InT>;
 
-        MISSamples(T value, const std::vector<Prec>& pdfs, int idx);
-    };
+        template<class T,class InT, class SampleT>
+        concept Sampler = Functor<T, InT,SampleT>;
 
-    template<class T>
-    MISSamples<T>::MISSamples(T value, const std::vector<Prec>& pdfs, int idx) 
-    : Value(value), Pdfs(pdfs), DistributionIdx(idx)
-    {
+        template<class T,class InT,class DistributionT, class SampleT>
+        concept DistSampler = Functor<T, InT,DistributionT,SampleT>;
+
+        template<class T, class InT, class DistributionT>
+        concept DistPdf = Functor<T,Prec,DistributionT,InT>;
+
+        template<class T, class DistributionT, class OutT, class InT>
+        concept DistFunc = Functor<T,DistributionT,OutT,InT>;
+
+        template<class T, class DistributionT>
+        concept DistRelativeCount = Functor<T,Prec,DistributionT>;
+
+        template<class T,class DistributionT, class InT, class DistributionRangeT,class DistRelativeCountT,class DistPdfT>
+        concept Weight = Functor<T,Prec,DistributionT,InT,DistributionRangeT,DistPdfT,DistRelativeCountT> 
+            && DistPdf<DistPdfT,InT,DistributionT>
+            && DistRelativeCount<DistRelativeCountT, DistributionT>
+            && std::ranges::forward_range<DistributionRangeT>;
         
-    }
+        template<class InT, std::ranges::forward_range DistributionRangeT, DistPdf<InT,int> DistPdfT, DistRelativeCount<int> DistRelativeCountT>
+        constexpr Prec BalanceHeuristic(int current_dist, InT x, DistributionRangeT dist_range,
+        DistPdfT dist_pdf, DistRelativeCountT dist_relative_count)
+        {
+            
+            auto summer = [&](Prec sum, int dist_idx) {return sum + dist_pdf(dist_idx,x)* dist_relative_count(dist_idx);};
+            Prec sum = std::accumulate(std::ranges::begin(dist_range),std::ranges::end(dist_range),0.f,summer);
 
-    //sample_count is the sample count per distribution
-
-    template<class MISSampleT, class WeightFuncT>
-    inline auto _MISUpdateSum(const MISSampleT& sample, Prec* inv_counts, Prec* relative_counts, WeightFuncT weight_func)
-    {
-        
-        auto weight = weight_func(&sample.Pdfs[0],&sample.Pdfs[sample.Pdfs.size()],relative_counts,sample.DistributionIdx);
-        return sample.Value/ sample.Pdfs[sample.DistributionIdx] * weight *inv_counts[sample.DistributionIdx];
-    }
-
-    template<class MISSampleIT , class WeightFuncT, class SampleValT>
-    inline SampleValT MultipleImportanceSampling(MISSampleIT samples_begin,const MISSampleIT samples_end, 
-    const int* sample_counts, const int distribution_count,SampleValT start_val, WeightFuncT weight_func) 
-    {
-        using namespace std::placeholders;
-        int full_sample_count = std::accumulate(sample_counts,sample_counts+distribution_count,0);
-        std::vector<Prec> inv_counts(distribution_count);
-        std::vector<Prec> relative_counts(distribution_count);
-
-        for(int i= 0; i< distribution_count;i++) {
-            inv_counts[i] = 1.f/(Prec)sample_counts[i] ;
-            relative_counts[i] = (Prec)sample_counts[i]/(Prec) full_sample_count ;
+            return dist_pdf(current_dist,x)* dist_relative_count(current_dist) / sum;
         }
 
-        return std::accumulate(samples_begin,samples_end,start_val,
-        [&](SampleValT sum, decltype(*samples_begin) samples){
-            return sum + _MISUpdateSum(samples,&inv_counts[0],&relative_counts[0],weight_func);
-            });
+        
+
+        template<
+            class DistributionT, 
+            class InT, 
+            class OutT,
+            class SampleT, 
+            std::ranges::forward_range DistributionRangeT, 
+            DistSampler<InT,DistributionT,SampleT> SamplerT,
+            DistFunc<DistributionT,OutT,InT> DistFuncT, 
+            DistPdf<InT,DistributionT> DistPdfT, 
+            DistRelativeCount<DistributionT> DistRelativeCountT,
+            Weight<DistributionT, InT, DistributionRangeT, DistRelativeCountT,DistPdfT> WeightT
+ 
+        > 
+        constexpr OutT MultipleImportanceSampling(OutT start_val, DistributionRangeT dist_range, SampleT* samples,int sample_count , SamplerT dist_sampler, 
+        DistFuncT dist_func, DistPdfT dist_pdf, DistRelativeCountT dist_relative_count, WeightT weight)
+        {
+            auto update_sum = [&](OutT sum, SampleT sample, DistributionT dist_idx) {
+                auto x = dist_sampler(dist_idx,sample);
+                auto f = dist_func(dist_idx,x);
+                auto p = dist_pdf(dist_idx,x);
+                auto w = weight(dist_idx,x,dist_range,dist_pdf,dist_relative_count);
+                return sum + f/p * w;
+            };
+
+            auto compute_dist_integral = [&](std::pair<OutT,SampleT*> sum, DistributionT dist_idx) -> std::pair<OutT,SampleT*>
+            {
+                OutT integral = sum.first; 
+                SampleT* current_sample = sum.second;
+                int dist_sample_count = dist_relative_count(dist_idx)*sample_count;
+                using namespace std::placeholders;
+                OutT dist_sum = std::accumulate(current_sample,current_sample+dist_sample_count,start_val, std::bind(update_sum,_1,_2,dist_idx));
+
+                return std::pair{integral+ dist_sum / (Prec) dist_sample_count, current_sample+dist_sample_count};
+            };
+
+            return std::accumulate(std::ranges::begin(dist_range),std::ranges::end(dist_range),
+                std::pair{start_val,samples},
+                compute_dist_integral ).first;
+
+
+        }
     }
-
-
-    Prec BalanceHeuristic(const Prec* pdf_first,const Prec* pdf_last, Prec* relative_count,int distribution_idx);
-
 }
